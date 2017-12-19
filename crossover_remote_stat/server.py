32f3580bae3a358c0d4b1846790702166e9d6ce5
Freed <buscando_alink@hotmail.com>
2017-12-19 17:56:11 -0400

    Change server behaviour

--- crossover_remote_stat/server.py
+++ crossover_remote_stat/server.py
@@ -1,54 +1,65 @@
 from datetime import datetime
 from cryptography.fernet import Fernet
 from flask import Flask, request
+from logging import getLogger
 from pickle import loads as pickle_loads
 from socket import socket, AF_INET, SOCK_STREAM
 from crossover_remote_stat.app.database.models import Client, ScanType, \
 										ScanResult, WindowsEventLog, Session 
 from crossover_remote_stat.app.xml_handler import XMLHandler
-from logging import getLogger
+from crossover_remote_stat.app.client_side.uploader import upload_and_execute
 
 
 log = getLogger(__name__)
 app = Flask(__name__)
 
-def initialize():
+def save_clients(clients):
 	"""Read xml looking for client pc"""
-	if XMLHandler.validate_xml():
-		# get all clients from xml
-		clients = XMLHandler.get_clients()
-		# get just clients ip address as a set
-		clients_ip = { c.get('ip') for c in XMLHandler.get_clients() }
-		# check which clients exist
-		existing_clients = Session.query(Client).filter(Client.ip_address.in_(clients_ip)).all()
-		# get the ip address of saved clients 
-		existing_clients_ip = { x.ip_address for x in existing_clients }
-		# determine which clients are new in the xml file
-		new_clients_ip = clients_ip - existing_clients_ip
-		# get the complete new client data
-		new_clients = [ c for c in clients if c.get('ip') in new_clients_ip]
 
-		# insert into db new clients
-		for d_client in new_clients:
-			client = Client.load_from_dict(d_client)
-			client.token = Fernet.generate_key()
-			try:
-				for alert in d_client.get('alert'):
-					scanresult = ScanResult()
-					scanresult.scan_type_key = alert.get('type')
-					scanresult.limit_value = alert.get('limit')
-					scanresult.client = client
-					Session.add(scanresult)
-				Session.add(client)
-				Session.commit()
-			except Exception as e:
-				print(e)
-				Session.rollback()
-				return False
-		return True
+	# get just clients ip address as a set
+	clients_ip = { c.get('ip') for c in XMLHandler.get_clients() }
+	# check which clients exist
+	existing_clients = Session.query(Client).filter(Client.ip_address.in_(clients_ip)).all()
+	# get the ip address of saved clients 
+	existing_clients_ip = { x.ip_address for x in existing_clients }
+	# determine which clients are new in the xml file
+	new_clients_ip = clients_ip - existing_clients_ip
+	# get the complete new client data
+	new_clients = [ c for c in clients if c.get('ip') in new_clients_ip]
+
+	# insert into db new clients
+	for d_client in new_clients:
+		client = Client.load_from_dict(d_client)
+		client.token = Fernet.generate_key()
+		try:
+			for alert in d_client.get('alert'):
+				scanresult = ScanResult()
+				scanresult.scan_type_key = alert.get('type')
+				scanresult.limit_value = alert.get('limit')
+				scanresult.client = client
+				Session.add(scanresult)
+			Session.add(client)
+			Session.commit()
+		except Exception as e:
+			print(e)
+			Session.rollback()
+			return False
+	return True
+
+def upload_client(clients):
+	if XMLHandler.validate_xml():
+		for client in clients:
+			upload_and_execute(client)
 	else:
-		print('No valid xml file')
-		return False
+		log.warning('No valid xml file')
+
+def initialize():
+	# get all clients from xml
+	clients = XMLHandler.get_clients()
+	clients_where_saved = save_clients(clients)
+
+	if clients_where_saved:
+		upload_client(clients)
 
 def save_statistics(client, statistics):
 	'''client = DbModel, statistics = dict'''
--- run.py
+++ run.py
@@ -1,10 +1,15 @@
+from crossover_remote_stat.app.xml_handler import XMLHandler
 from crossover_remote_stat import server, config
 
 # get port from config file
 PORT = int(config['SERVER']['PORT'])
 
 # add new clients into xml file to db
-server.initialize()
+
+clients = XMLHandler.get_clients()
+was_initialized = server.save_clients(clients)
+if was_initialized:
+	server.upload_client(clients)
 
 # run http rest flask server
-server.app.run(debug=True,port=PORT)
+# server.app.run(debug=True,port=PORT)
