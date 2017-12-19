01ecd817cb2c9d15173b0e611c62bb951bcf2216
Freed <buscando_alink@hotmail.com>
2017-12-18 20:11:53 -0400

    Add comments

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from CrossOverAssignment import server, config
-
+from crossover_remote_stat import server, config
+	
+# get port from config file
 PORT = int(config['SERVER']['PORT'])
 
+# add new clients into xml file to db
 server.initialize()
+
+# run http rest flask server
 server.app.run(debug=True,port=PORT)
